59182c65f72011a86d3cfe6b542a19d4d027b3a3
江上风平浪静 <jn90@163.com>
2018-04-24 16:56:34 +0800

    重新梳理了一下
    重新梳理了一下

--- app.js
+++ app.js
@@ -1,133 +1,61 @@
-// 加载依赖包
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const flash =  require('connect-flash');
-const config = require('./config/default');
-const routers = require("./routes");
+/**
+ * 加载依赖组件包
+ */
+
+const express = require('express'); //必须用的express
+const compression = require('compression'); //好像可以压缩
+const session = require('express-session'); //session管理
+const bodyParser = require('body-parser'); //解析表单
+const logger = require('morgan'); //日志组件
+const chalk = require('chalk'); //改变console颜色的组件
+const errorHandler = require('errorhandler'); //开发环境中的错误中间件
+const lusca = require('lusca'); //增加安全性的中间件
+const dotenv = require('dotenv'); //从env的文本文件中，解析出来好东西
+const MongoStore = require('connect-mongo')(session); //把session 存到mongodb
+const flash = require('express-flash'); //一次性提示消息
+const path = require('path'); //路径
+const mongoose = require('mongoose'); //数据库
+const passport = require('passport');
+const expressValidator = require('express-validator');
+const expressStatusMonitor = require('express-status-monitor');
+const sass = require('node-sass-middleware');
+const multer = require('multer');
+
+const upload = multer({ dest: path.join(__dirname, 'uploads') });
+
+/**
+ * 加载环境文件
+ */
+dotenv.load({path:'.env.example'});
+
+/**
+* 路由的目录引入, 先引入了4个路由
+*/
+const homeController= require('./controllers/home')
+const userController = require('./controllers/user');
+const apiController = require('./controllers/api');
+const contactController = require('./controllers/contact');
 
 
-
-
+/**
+ * 创建express的app
+ */
 
 const app = express();
 
-//设置存放模板的目录
-app.set('views',path.join(__dirname, 'views'));
-//设置模板引擎为ejs
-app.set('view engine', 'ejs');
 
 
-// 托管静态文件，这个目录下面的文件可以直接访问了 app.use(express.static("public"));
-// 如/public/bootstrap.css   输入网址 localhost:1234/bootstrap.css
-// 如果第一个参数是目录的话，就会创造虚拟路径，也就是localhost:1234/public/bootstrap.css 才能访问
-app.use("/public",express.static(path.join(__dirname, "public")));
+/**
+* 主要路由
+*/
+app.get('/', homeController.index);
+app.get('/signup', userController.getSignup);
+app.post('/signup', userController.postSignup);
 
+/**
+* 开启服务器
+*/
 
-
-//加载mongoose操作数据库
-// const Mongolass = require('mongolass');
-// const Schema = Mongolass.Schema
-//连接数据库
-//const mongolass = new Mongolass("mongodb://localhost:8889/myblog");
-//表结构
-// const UserSchema = new Schema('UserSchema', {
-//   username: { type: 'string', required: true },
-//   password: { type: 'string' }
-// })
-//定义user这个表/collection  用这个结构
-// const User = mongolass.model('User', UserSchema);
-// User.index({username:1},{unique:true}).exec();
-
-//session 中间件
-app.use(session({
-  name: config.session.key, //设置cookie保存的字段id名称
-  secret: config.session.secret, //设置secret计算hash值存在cookie中 防篡改
-  saveUninitialized: false, //设置为false，强制创建一个session，即使用户未登录
-  resave:true, //强制更新session
-  cookie:{
-    maxAge:config.session.maxAge //过期时间，过期后cookie中的session id自动删除
-  },
-  store: new MongoStore({ //将session存储到mongodb
-    url: config.mongodb
-  })
-}))
-
-
-// flash 中间件，用来显示通知
-app.use(flash())
-
-//处理表单以及文件上传的中间件
-app.use(require('express-formidable')({
-  uploadDir: path.join(__dirname,'public/img'),  //上传文件目录
-  keepExtensions: true //保留后缀
-}));
-
-// 设置模板全局常量
-
-app.locals.blog = {
-  title : "cnki-pro",
-  description: "this is cnki blog"
-}
-
-
-//添加模板必须的三个变量
-app.use(function(req,res,next){
-  res.locals.user = req.session.user
-  res.locals.success = req.flash('success').toString()
-  res.locals.error = req.flash('error').toString()
-  next()
-})
-//路由
-routers(app)
-
-// app.get('/gogogo',function(req,res,next){
-//   req.session.user="wa lala  my name is user";
-//   console.log(config1.info);
-//   res.render('index');
-// });
-// app.get('/publish',function(req,res,next){
-//   res.render('publish');
-// });
-// app.get('/login',function(req,res,next){
-//   res.render('login');
-// });
-
-
-// function check(req,res,next){
-//   next();
-// }
-// app.get('/register',check,function(req,res,next){
-//   console.log(req.session.user);
-//   console.log(req.session.value);
-//   console.log(req.session.user);
-//   res.render('register',{username:'lucy'});
-// });
-
-
-
-// app.get('/api/register',function(req,res,next){
-//   console.log(req.query);
-//   res.send("aaa");
-// });
-
-// app.post('/api/register',function(req,res,next){
-//   res.send("aaa");
-//   const username =  req.fields.username;
-//   const password =  req.fields.password;
-//   let userdata ={
-//     username : username,
-//     password : password
-//   }
-//   console.log(userdata);
-//   User.create(userdata).exec().then(console.log("gogogo")).catch(function(e){
-//     console.error(e);
-//   });
-//   res.send("bbb");
-// });
-
-//监听端口，启动程序
-app.listen(3001,function(){
-    console.log("服务器开启^-^开始监听3001端口")
-})
+app.listen(8889, () => {
+  console.log("服务器开始了 8889");
+});
---
+++ controllers/home.js
@@ -0,0 +1,8 @@
+/**
+ * GET /
+ * 主页
+ */
+
+exports.index = (req,res) => {
+  res.send('this is a home page');
+}
